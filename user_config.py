3dbc75c855e2e6691a0741b30cbb7c851af1bc05
zhuxu <33270827+zhuxu222@users.noreply.github.com>
2024-03-28 16:51:39 +0800

    Update user_config.py
    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,1 +1,12 @@
 
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
